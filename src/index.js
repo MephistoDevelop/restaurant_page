18cdbb0f936aeedbc2dd7a76468718d4a30001a5
Cristian <inesherald@gmail.com>
2019-12-23 10:45:10 -0600

    Fix Menu styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 function bodyComponent() {
   const element = document.createElement("div");
   element.className = "main-container";
-  element.innerHTML = "Hello, i'm MephistoDevelop again !!";
   element.style.display = "flex";
   element.style.color = "white";
   element.style.height = "100vh";
@@ -18,17 +17,23 @@ function addNavbar() {
   const navContainer = document.createElement("div");
   navContainer.className = "tabscontainer";
   navContainer.style.display = "flex";
-  navContainer.style.width = "100vw";
+  navContainer.style.width = "100%";
+  navContainer.style.backgroundColor = "blue";
+
   const nav = document.querySelector(".main-container");
   nav.appendChild(navContainer);
+
   const tabs = ["Home", "Menu", "Contact"];
   tabs.forEach(tab => {
     const button = document.createElement("button");
     button.addEventListener("click", changeTab);
     button.textContent = tab;
     button.classList.add(tab.toLowerCase());
-    button.style.width = "29%";
-    const tabsContainer = document.createElement("div");
+    button.style.width = "33.5%";
+    button.style.height = "100%";
+    button.style.border = "none";
+    button.style.fontSize = "21px";
+    button.style.padding = "10px 0";
     navContainer.appendChild(button);
   });
 }
