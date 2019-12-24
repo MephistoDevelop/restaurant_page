2edb037a31a21f45d4ce18f558cab52180b1913b
Cristian <inesherald@gmail.com>
2019-12-24 02:51:19 -0600

    finished home page

--- src/index.js
+++ src/index.js
@@ -30,9 +30,25 @@ function changeTab() {
     const mainContainer = document.querySelector(".main-container");
     const textHome = document.createElement("div");
     const mainText = document.createElement("h1");
+    const maintext2 = document.createElement("p");
+    const maintext3 = document.createElement("p");
+    const maintext4 = document.createElement("p");
+    const maintext5 = document.createElement("p");
+    const maintext6 = document.createElement("p");
+    maintext2.textContent = "-variety of dishes";
+    maintext3.textContent = "-Fresh eats";
+    maintext4.textContent = "-Live Music";
+    maintext6.textContent = "Open everydays from 10am";
+    maintext5.textContent =
+      "Try the Speciality of the house Tostada de Ceviche";
     textHome.className = "textHome";
     mainText.textContent = "SeaFood Hernandez";
     textHome.appendChild(mainText);
+    textHome.appendChild(maintext2);
+    textHome.appendChild(maintext3);
+    textHome.appendChild(maintext4);
+    textHome.appendChild(maintext5);
+    textHome.appendChild(maintext6);
     mainContainer.appendChild(textHome);
   } else if (this.className === "menu") {
     alert("Yo soy la tab Menu");
