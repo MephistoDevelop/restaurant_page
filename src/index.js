ffb0c7040d70d58487fdc929c3acdfd926f5963f
Cristian <inesherald@gmail.com>
2019-12-24 01:26:04 -0600

    remove styles properties on index.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,12 @@
 function bodyComponent() {
   const element = document.createElement("div");
   element.className = "main-container";
-  element.style.display = "flex";
-  element.style.color = "white";
-  element.style.height = "100vh";
-  element.style.display = "flex";
-  element.style.margin = "10px";
-  element.style.padding = "10px";
-  element.style.backgroundColor = "#dff6f0";
-  element.style.flexDirection = "column";
   return element;
 }
 
 function addNavbar() {
   const navContainer = document.createElement("div");
   navContainer.className = "tabscontainer";
-  navContainer.style.display = "flex";
-  navContainer.style.width = "100%";
 
   const nav = document.querySelector(".main-container");
   nav.appendChild(navContainer);
@@ -28,14 +18,7 @@ function addNavbar() {
     button.addEventListener("click", changeTab);
     button.textContent = tab;
     button.classList.add(tab.toLowerCase());
-    button.style.width = "33.5%";
-    button.style.height = "100%";
-    button.style.border = "none";
-    button.style.fontSize = "28px";
-    button.style.padding = "10px 0";
-    button.style.fontWeight = "800";
-    button.style.backgroundColor = "transparent";
-    button.style.textShadow = `${1}px ${1}px ${1}px white`;
+
     navContainer.appendChild(button);
   });
 }
