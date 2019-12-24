955d79c50d6796a4f9ab855aa1dcf2f221c3a306
Cristian <inesherald@gmail.com>
2019-12-24 02:13:05 -0600

    Append text child to home text contrainer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 //Page to
+let flag = 0;
 function bodyComponent() {
   const element = document.createElement("div");
   element.className = "main-container";
@@ -24,7 +25,20 @@ function addNavbar() {
 }
 
 function changeTab() {
-  alert("Haz cambiado y clickeado  la tab");
+  if (this.className === "home" && flag === 0) {
+    flag++;
+    const mainContainer = document.querySelector(".main-container");
+    const textHome = document.createElement("div");
+    const mainText = document.createElement("h1");
+    textHome.className = "textHome";
+    mainText.textContent = "SeaFood Hernandez";
+    textHome.appendChild(mainText);
+    mainContainer.appendChild(textHome);
+  } else if (this.className === "menu") {
+    alert("Yo soy la tab Menu");
+  } else if (this.className === "contact") {
+    alert("Yo soy la tab contact");
+  }
 }
 
 document.body.appendChild(bodyComponent());
